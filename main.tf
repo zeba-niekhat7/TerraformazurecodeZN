0053d6d3c75495756d8aed5c55840eb2bf27246f
zeba-niekhat7 <153175019+zeba-niekhat7@users.noreply.github.com>
2023-12-07 14:16:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,43 @@
-#Instance deployment
-module "ec2-Instance" {
-  source                          = "app.terraform.io/Motiva-ServiceNow/ec2-Instance/aws"
-  version                         = "1.0.7"
-  ami                             = var.golden_image_AMI_id
-  instance_type                   = var.instance_type
-  key_name                        = var.key_name
-  vpc_security_group_ids          = [data.aws_security_group.windows_sg.id]
-  cpu_credits                     = "unlimited"
-  associate_public_ip_address     = false
-  subnet_id                       = var.subnet_id
-  enable_volume_tags              = var.enable_volume_tags
-  # DOMAIN AUTO_JOIN!! the machine should join the domain by using tags
-  iam_instance_profile            = var.iam_instance_profile 
-  tags = merge({"windows_ad_join" = "${var.windows_ad_join}",
-                "os_platform"     = "Windows",
-                "ad_name"         = var.ad_name,
-                "Patch_Group"     = var.patch_group,
-                "backup_schedule" = var.backupplan,
-                "Name"            = "${var.is_non_standard_vm ? var.non_standard_host_name : local.host_name}",
-                "host_name"       = "${var.is_non_standard_vm ? var.non_standard_host_name : local.host_name}"
-              }, local.tags)
-
-  volume_tags                     = merge({"Name": "${var.is_non_standard_vm ? var.non_standard_host_name : local.host_name}-data-disk"}, local.tags)
-  root_block_device               = [{
-    volume_size                   = var.root_volume_size
-    volume_type                   = var.root_volume_type
-    iops                          = var.root_iops
-    throughput                    = var.root_throughput
-    delete_on_termination         = true
-  }]
-  ebs_block_device                = var.ebs_block_device
+module "windowsvm" {
+  source                = "app.terraform.io/Motiva-ServiceNow/windowsvm/azure"
+  version               = "1.0.0"
+  resourceGroup         = var.resourceGroup
+  vmName                = var.vmName
+  imagePublisher        = var.imagePublisher
+  imageOffer            = var.imageOffer
+  imageSKU              = var.imageSKU
+  imageVersion          = var.imageVersion
+  location              = var.location
+  vnet                  = var.vnet
+  subnet                = var.subnet
+  vnetRG                = var.vnetRG
+  keyVault              = var.keyVault
+  keyVaultRG            = var.keyVaultRG
+  userName              = var.userName
+  storageAccount        = var.storageAccount
+  vmSize                = var.vmSize
+  adDomain              = var.adDomain
+  availabilityZone      = var.availabilityZone
+  dataDisks             = var.dataDisks
+  provisionedBy         = var.provisionedBy
+  classOfBusiness	      = var.classOfBusiness
+  applicationName       = var.applicationName
+  dataClassification    = var.dataClassification
+  businessImpact	      = var.businessImpact
+  operatingEnvironment	= var.operatingEnvironment
+  securityZone	        = var.securityZone
+  costCenter	          = var.costCenter
+  supportGroup          = var.supportGroup
+  backUpPlan            = var.backUpPlan
+  adJoin                = var.domainJoin
+  licenseType           = var.licenseType
+  KEYVAULT_ARM_SUBSCRIPTION_ID  = var.KEYVAULT_ARM_SUBSCRIPTION_ID
+  KEYVAULT_ARM_TENANT_ID        = var.KEYVAULT_ARM_TENANT_ID
+  KEYVAULT_ARM_CLIENT_ID        = var.KEYVAULT_ARM_CLIENT_ID
+  KEYVAULT_ARM_CLIENT_SECRET_ID = var.KEYVAULT_ARM_CLIENT_SECRET_ID
+  RESOURCE_ARM_SUBSCRIPTION_ID  = var.RESOURCE_ARM_SUBSCRIPTION_ID
+  RESOURCE_ARM_TENANT_ID        = var.RESOURCE_ARM_TENANT_ID
+  RESOURCE_ARM_CLIENT_ID        = var.RESOURCE_ARM_CLIENT_ID
+  RESOURCE_ARM_CLIENT_SECRET_ID = var.RESOURCE_ARM_CLIENT_SECRET_ID
 }
+  
